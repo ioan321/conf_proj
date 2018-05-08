37625f6c63767f77d9665756bcd0fa405cf103ee
= <michael.deiters@infosys.com>
2018-05-08 14:24:10 -0400

    Added RouterModule, Routes, ActivatedRoute, and BookComponent. Also started creating routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -10,16 +12,25 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SuperTableComponent } from './super-table/super-table.component';
+import { BookComponent } from './book/book.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'book', pathMatch: 'full' },
+  { path: 'book', component: BookComponent }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     DashboardComponent,
-    SuperTableComponent
+    SuperTableComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     LayoutModule,
