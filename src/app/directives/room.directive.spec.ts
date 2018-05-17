db153a98c54bf16a8341cb1176c7639c23cf122b
Michael Deiters <michael.deiters@infosys.com>
2018-05-17 09:55:44 -0400

    Built the room directive

---
+++ src/app/directives/room.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RoomDirective } from './room.directive';
+
+describe('RoomDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RoomDirective();
+    expect(directive).toBeTruthy();
+  });
+});
